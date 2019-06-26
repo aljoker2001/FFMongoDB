1375c76970a7d33572a54191ed2316b93742cc35
aljoker2001 <aljoker2001@hotmail.com>
2019-06-25 19:57:39 -0700

    added URI information for heroku deployement

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,42 @@
 module.exports = function (app) {
   var mongojs = require('mongojs')
+  var mongodb = require('mongodb')
   var databaseUrl = 'frienddb'
   var collections = ['friends']
+  let uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/frienddb'
 
   // Use mongojs to hook the database to the db variable
   var db = mongojs(databaseUrl, collections)
 
   // This makes sure that any errors are logged if mongodb runs into an issue
-  db.on('error', function (error) {
-    console.log('Database Error:', error)
-  })
-  app.get('/api/friends', function (req, res) {
-    db.friends.find({}, function (err, data) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.json(data)
-      }
+  mongodb.MongoClient.connect(uri, function (err, client) {
+    if (err) throw err
+    db.on('error', function (error) {
+      console.log('Database Error:', error)
     })
-  })
+    app.get('/api/friends', function (req, res) {
+      db.friends.find({}, function (err, data) {
+        if (err) {
+          console.log(err)
+        } else {
+          res.json(data)
+        }
+      })
+    })
 
-  app.post('/api/friends', function (req, res) {
+    app.post('/api/friends', function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of the body parsing middleware
-    var newFriend = req.body
-    console.log('new friend:', newFriend)
-    db.friends.insert(newFriend, function (err, data) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.json(data)
-      }
+      var newFriend = req.body
+      console.log('new friend:', newFriend)
+      db.friends.insert(newFriend, function (err, data) {
+        if (err) {
+          console.log(err)
+        } else {
+          res.json(data)
+        }
+      })
     })
+    client.close()
   })
 }
